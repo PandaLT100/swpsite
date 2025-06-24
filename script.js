1b70dd895971ed0ef0b6aca74d00873bf21045c1
PandaLT100 <itzel.dominguez01@gmail.com>
2025-06-24 10:30:01 -0700

    corrected typos
    corrected typos in lesson and added if else statements to questions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Lesson 1 Quiz//
+
 function checkAnswer() {
     let form = document.getElementById('qone');
     let userAnswer = form.answer.value;
@@ -9,3 +11,35 @@ if (userAnswer === correctAnswer) {
     document.getElementById('resulta').innerText = "Incorrect. Try again! :D"
 }
 }
+
+function checkAn() {
+    let formtwo = document.getElementById('qtwo');
+    let usAnswer = formtwo.answer.value;
+    let correctAn = 'yusted';
+
+    if (usAnswer === correctAn) {
+        document.getElementById('resultb').innerText = "Correct!"
+    } else {
+        document.getElementById('resultb').innerText = "Incorrect :(. Keep trying!"
+    }
+    }
+
+function checkAns() {
+    let formthree = document.getElementById('qthree');
+    let unan = formthree.answer.value;
+    let correct = 'estoybien';
+    let soy = 'soybien';
+    let mal = 'toymal';
+    let chau = 'minombre';
+
+    if (unan === correct) {
+        document.getElementById('resultc').innerText = "Correcto!"
+    } else if (unan === soy) {
+        document.getElementById('resultc').innerText = "So close!"
+    } else if (unan === mal) {
+        document.getElementById('resultc').innerText = "You good?"
+    } else if (unan === chau) {
+        document.getElementById('resultc').innerText = ">:("
+    }
+}
+
